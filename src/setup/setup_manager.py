4376f62dd569d95c2cc274879333e6bea580ff98
NoobAndCoding <167626518+NoobAndCoding@users.noreply.github.com>
2025-02-17 16:12:57 +0000

    bug fixes

--- src/setup/setup_manager.py
+++ src/setup/setup_manager.py
@@ -2,7 +2,6 @@ import nextcord
 from json import loads
 from pathlib import Path
 from dotenv import load_dotenv
-from yaml import load
 
 config_env = load_dotenv("src/setup/.env")
 config_json = None
